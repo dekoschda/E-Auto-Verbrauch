e479b71de5e5b83a82af9a388c1fcf9390938c8b
dekoschda <de.koschda@gmail.com>
2026-09-04 22:17:35 +0200

    v46

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='e-auto-v45-unified-icons';
+const CACHE_NAME='e-auto-v46-mobile-history-fix';
 self.addEventListener('install',event=>self.skipWaiting());
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>caches.match(event.request))));
